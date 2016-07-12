4d0d1774687086c12bf0c52b7576994e3d55f4fd
zimmat <zimkhitha@projectcodex.co>
2016-07-12 10:06:31 +0200

    deploying

--- express-app/server.js
+++ express-app/server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var exphbs = require('express-handlebars');
 var app = express();
 var handlebars = require('handlebars');
-var fs = require('fs');
 var makeObject = require('../makeObject');
 var getSalesData = require('../getSalesData');
 var getPurchases = require('../getPurchases');
@@ -14,7 +13,6 @@ var popularCategory = require('../popularCategory');
 var leastPopularCat = require('../leastPopularCat');
 var mostProfitableCat = require('../mostProfitableCat');
 var mostProfitableProduct = require('../mostProfitableProduct');
-
 var firstWeek = ['01-Feb', '02-Feb', '03-Feb', '04-Feb', '05-Feb', '06-Feb', '6-Feb', '07-Feb'];
 var cats = {
   'Milk 1l': 'Dairy',
@@ -36,41 +34,48 @@ var cats = {
   'Rose (plastic)': 'other',
   'Valentine Cards': 'other'};
 
-var weekOneQuantity = makeObject('../files/week1.csv');
-var weekOneSales = getSalesData('../files/week1.csv');
-var weekOnePurchases = getPurchases('../files/purchases.csv', firstWeek);
-var weekOneQuantityCategories = makeCategories(weekOneQuantity,cats);
-var weekOneSalesCat = makeCategories(weekOneSales,cats);
-var weekOnePurchaseCat = makeCategories(weekOnePurchases,cats);
-var weekOneProfit = profitMap(weekOneSales,weekOnePurchases);
-var weekOneProfitCategories = profitMap(weekOneSalesCat,weekOnePurchaseCat);
+var weeklyStats = function(path,purchases){
+  var weekOneQuantity = makeObject(path);
+  var weekOneSales = getSalesData(path);
+  var weekOnePurchases = getPurchases(purchases, firstWeek);
+  var weekOneQuantityCategories = makeCategories(weekOneQuantity,cats);
+  var weekOneSalesCat = makeCategories(weekOneSales,cats);
+  var weekOnePurchaseCat = makeCategories(weekOnePurchases,cats);
+  var weekOneProfit = profitMap(weekOneSales,weekOnePurchases);
+  var weekOneProfitCategories = profitMap(weekOneSalesCat,weekOnePurchaseCat);
+  var MostpopularProduct = popularProduct(weekOneQuantity);
+  var UnpopularProduct = leastPopularProduct(weekOneQuantity);
+  var mostPopularCat = popularCategory(weekOneQuantityCategories);
+  var unpopularCat = leastPopularCat(weekOneQuantityCategories);
+  var profitableProduct = mostProfitableProduct(weekOneProfit);
+  var profitableCategory = mostProfitableCat(weekOneProfitCategories);
+  var data_for_template = {
+    popular: [MostpopularProduct,UnpopularProduct,mostPopularCat,unpopularCat],
+     profit:[profitableProduct,profitableCategory]
+  };
+  return data_for_template;
+}
 
-var popularProduct = popularProduct(weekOneQuantity);
-var leastPopularProduct = leastPopularProduct(weekOneQuantity);
-var mostPopularCat = popularCategory(weekOneQuantityCategories);
-var leastPopularCat = leastPopularCat(weekOneQuantityCategories);
-var profitableProduct = mostProfitableProduct(weekOneProfit);
-var profitableCategory = mostProfitableCat(weekOneProfitCategories);
+// var week1 = weeklyStats('../files/week1.csv', '../files/purchases.csv',firstWeek);
 
 
 app.engine('handlebars', exphbs({defaultLayout: 'display'}));
 app.set('view engine', 'handlebars');
 
-var data_for_template = {
-  popular: [popularProduct,leastPopularProduct,mostPopularCat,leastPopularCat],
-   profit:[profitableProduct,profitableCategory]
-};
-
-// create a route
-app.get('/sales/week1', function (req, res) {
- res.render('home', data_for_template);
+app.get('/', function(req, res){
+  res.render('home')
 });
-//start the server
-var server = app.listen(3000, function () {
+// create a route
+app.get('/sales/:week', function (req, res) {
+  var week = req.params.week;
+  var weeklyData = '../files/' + week +".csv"
+  var data = weeklyStats(weeklyData,'../files/purchases.csv');
+ res.render('weeklyStatistics', data );
+});
 
- var host = server.address().address;
- var port = server.address().port;
-
- console.log('Example app listening at http://%s:%s', host, port);
-
+//set the port number to an existing environment variable PORT or default to 5000
+app.set('port', (process.env.PORT || 5000));
+//start the app like this:
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
