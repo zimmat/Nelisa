e9f6bf7b5e664f01a78dd77576c40e433ce15f93
zimmat <zimkhitha@projectcodex.co>
2016-08-02 10:03:31 +0200

    add

--- insert_categories.js
+++ insert_categories.js
@@ -24,23 +24,3 @@ var values = [
 conn.query(sql, [values], function(err) {
     if (err) throw err;
   });
-
-  var productCategoryMap = {
-    'Milk 1l': 'Dairy',
-    'Imasi': 'Dairy',
-    'Bread': 'Bakery',
-    'Chakalaka Can': 'Canned Food',
-    'Gold Dish Vegetable Curry Can': 'Canned Food',
-    'Fanta 500ml': 'Soft-Drinks',
-    'Coke 500ml': 'Soft-Drinks',
-    'Cream Soda 500ml': 'Soft-Drinks',
-    'Iwisa Pap 5kg': 'Groceries',
-    'Top Class Soy Mince': 'Groceries',
-    'Shampoo 1 litre': 'toiletries',
-    'Soap Bar': 'toiletries',
-    'Bananas - loose': 'Fruit ',
-    'Apples - loose': 'Fruit ',
-    'Mixed Sweets 5s': 'Candy',
-    'Heart Chocolates': 'Candy',
-    'Rose (plastic)': 'Other  ',
-    'Valentine Cards': 'Other  '};
--- nelisa.sql
+++ nelisa.sql
@@ -17,11 +17,11 @@ create table products (
 )ENGINE=INNODB;
 create table sales (
     sales_id int primary key auto_increment, Sales_date date not null,
-        product_name char(100) not null, quantity int, sales_price decimal,
+        product_name char(100) not null, quantity int, sales_price decimal(10,2),
     product_id int,foreign key (product_id) references products(product_id)
 )ENGINE=INNODB;
 create table purchases (
     purchase_id int primary key auto_increment,shopName char(100) not null, purchase_date date not null,
-        product_name char(100) not null, quantity int, cost decimal,totalCost decimal,
+        product_name char(100) not null, quantity int, cost decimal(10,2),totalCost decimal(10,2),
     product_id int,foreign key (product_id) references products(product_id)
 )ENGINE=INNODB;
