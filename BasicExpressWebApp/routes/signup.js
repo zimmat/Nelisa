75fbe28501be2e83db4b209d5b8be270406a0216
zimmat <zimkhitha@projectcodex.co>
2016-10-21 12:17:17 +0200

    generated hash for users passwords

--- BasicExpressWebApp/routes/signup.js
+++ BasicExpressWebApp/routes/signup.js
@@ -1,11 +1,7 @@
 var bcrypt = require('bcrypt');
-const saltRounds = 10;
-const myPlaintextPassword = 's0/\/\P4$$w0rD';
-const someOtherPlaintextPassword = 'not_bacon';
-var salt = bcrypt.genSaltSync(saltRounds);
-var hash = bcrypt.hashSync(myPlaintextPassword, salt);
-exports.showSignup = function(req, res){
-	res.render('signup');
+
+exports.showSignup = function(req, res) {
+  res.render('signup');
 }
 
 exports.add = function(req, res, next) {
@@ -13,14 +9,18 @@ exports.add = function(req, res, next) {
     if (err) return next(err);
     var data = {
       username: req.body.username,
-      password: req.body.salt,
-      Role: req.body.Role
-
+      password: req.body.password,
+				Role: req.body.Role,
     };
+    console.log(data);
+    var password = req.body.password;
+    bcrypt.hash(password, 10, function(err, hash) {
+            data.password = hash;
 
     connection.query('insert into users set ?', data, function(err, results) {
-      if (err) return next(err);
+      if (err) return next(err)
       res.redirect('/login');
-    });
+});
   });
+});
 };
